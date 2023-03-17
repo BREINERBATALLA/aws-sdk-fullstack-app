bdf6f1a4dabc8ffbe9adfcfbaaf37a4fc92a5132
BREINERBATALLA <breiner.batalla@correounivalle.edu.co>
2023-03-17 14:05:09 -0500

    update

--- frontend/angular-app/src/app/task.service.ts
+++ frontend/angular-app/src/app/task.service.ts
@@ -10,21 +10,11 @@ export class TaskService {
   constructor(private http : HttpClient) { }
 
   getAll() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*' // reemplaza "*" con tu origen permitido si no deseas permitir todas las solicitudes
-      })
-    };
-    return this.http.get(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`,httpOptions)
+    return this.http.get(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`)
   }
   
   create(task: any){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*' 
-      })
-    };
-    return this.http.post(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`,httpOptions, task)
+    return this.http.post(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`, task)
   }
 
   update(id: String, task:any){
