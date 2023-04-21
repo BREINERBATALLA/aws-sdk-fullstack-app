43cd5223fff71d3ca33cc1c77456649919b5c125
BREINERBATALLA <breiner.batalla@correounivalle.edu.co>
2023-04-21 14:51:42 -0500

    update

--- frontend/angular-app/src/app/task.service.ts
+++ frontend/angular-app/src/app/task.service.ts
@@ -1,29 +1,30 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
+  private urlApiGateway: string = environment.apiUrl
 
   constructor(private http : HttpClient) { }
 
   getAll() {
-    return this.http.get(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`)
+    return this.http.get(`https://${this.urlApiGateway}/tasks`)
   }
   
   create(task: any){
 
-    return this.http.post(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks`, task)
+    return this.http.post(`https://${this.urlApiGateway}/tasks`, task)
   }
 
   update(id: String, task:any){
 
-    return this.http.put(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks/`+id, task)
+    return this.http.put(`https://${this.urlApiGateway}/tasks`+id, task)
   }
 
   delete(id: String) {
-    return this.http.delete(`https://k0al33e307.execute-api.us-east-1.amazonaws.com/prod/tasks/` + id)
+    return this.http.delete(`https://${this.urlApiGateway}/tasks`+id)
   }
 }
